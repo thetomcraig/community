ff981b0675c1dc7a5a8f60eca4ba741bbdca8fc0
Tom Craig <thetomcraig@icloud.com>
2025-07-07 08:46:54 -0700

    Alfred clipboard stuff

--- apps/alfred_app/alfred_app.py
+++ apps/alfred_app/alfred_app.py
@@ -3,20 +3,23 @@ from talon import Context, Module, actions
 ctx = Context()
 mod = Module()
 
-mod.tag("alfred_clipboard_search")
+# mod.tag("alfred_clipboard")
 
 @ctx.action_class("user")
 class UserActions:
     def alfred_search():
         actions.key("cmd-space")
-        
-    def alfred_clipboard_search():
+
+    def alfred_clipboard(text: str = None):
         actions.key("alt-space")
+        if text is not None:
+            actions.sleep("100ms")
+            actions.insert(text)
 
 @mod.action_class
 class Actions:
     def alfred_search():
         """Search with Alfred"""
-        
-    def alfred_clipboard_search():
+
+    def alfred_clipboard(text: str = None):
         """Search Alfred clipboard"""
--- apps/alfred_app/alfred_app.talon
+++ apps/alfred_app/alfred_app.talon
@@ -2,4 +2,5 @@ os: mac
 mode: command
 -
 launcher | alfred: user.alfred_search()
-clip show | show clip | show clipboard | alfred clipboard : user.alfred_clipboard_search()
+(clip | clipboard): user.alfred_clipboard()
+(clip | clipboard) search [<user.text>]: user.alfred_clipboard("{text}")
